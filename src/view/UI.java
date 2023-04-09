f376cc8b4bdebc615aae143138d88d1307668955
Raúl <raulduranc@hotmail.com>
2023-04-09 21:12:45 +0200

    Jframe1

--- src/view/UI.java
+++ src/view/UI.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
 
 public class UI extends JFrame {
 
@@ -15,11 +21,34 @@ public class UI extends JFrame {
 	 */
 	public UI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 543, 397);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenuItem mnitInicar = new JMenuItem("Iniciar");
+		menuBar.add(mnitInicar);
+		
+		JMenu mnLevels = new JMenu("Niveles");
+		menuBar.add(mnLevels);
+		ButtonGroup group = new ButtonGroup();
+		
+		JCheckBoxMenuItem mnitFacil = new JCheckBoxMenuItem("Facil");
+		mnLevels.add(mnitFacil);
+		mnLevels.add(mnitFacil);
+		
+		JCheckBoxMenuItem mnitMedio = new JCheckBoxMenuItem("Medio");
+		mnLevels.add(mnitMedio);
+		
+		JCheckBoxMenuItem mnitDificil = new JCheckBoxMenuItem("Dificil");
+		mnLevels.add(mnitDificil);
+		
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 	}
 
-}
+}	
