5f1cf4d8f2128c849f10653de41e4a6c5bed17a0
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-04-09 20:28:18 +0200

    Create UI, APP

---
+++ src/crontoller/APP.java
@@ -0,0 +1,20 @@
+package crontoller;
+
+import java.awt.EventQueue;
+
+import view.UI;
+
+public class APP {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
